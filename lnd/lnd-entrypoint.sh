10a209c61f85038b2aaabb3a19f1fd441e47a81c
0xStorm <waldenj6@proton.me>
2023-03-18 15:52:24 +0800

    lnd

--- lnd/lnd-entrypoint.sh
+++ lnd/lnd-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-source /usr/local/bin/wait-for-bitcoind.sh
+echo Waiting for bitcoind to start...
+until curl --silent --user regtest:regtest --data-binary '{"jsonrpc": "1.0", "id": "lnd-node", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://bitcoind:18443/ | jq -e ".result.blocks > 0" > /dev/null 2>&1
+do
+	echo -n "."
+	sleep 1
+done
 
 echo Starting lnd...
 lnd --lnddir=/lnd --noseedbackup > /dev/null &
@@ -11,7 +16,12 @@ do
 	sleep 1
 done
 echo "Startup complete"
-echo "Funding lnd wallet"
-source /usr/local/bin/fund-lnd.sh
+# echo "Funding lnd wallet"
+# source /usr/local/bin/fund-lnd.sh
+address=$(lncli --lnddir=/lnd --network regtest newaddress np2wkh | jq .address)
+echo "================================================"
+echo "LND Wallet address: " ${address}
+echo "================================================"
 
-exec "$@"
+# exec "$@"
+tail -f /lnd/logs/bitcoin/regtest/lnd.log
